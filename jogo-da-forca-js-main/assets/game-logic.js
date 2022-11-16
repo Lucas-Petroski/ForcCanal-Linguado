e9c6b545f13383e37ba0b26493a4983923f1f4d8
Lucas Petroski <112898100+Lucas-Petroski@users.noreply.github.com>
2022-11-15 23:47:44 -0300

    Add files via upload

---
+++ jogo-da-forca-js-main/assets/game-logic.js
@@ -0,0 +1,110 @@
+// variables
+let words = [
+  'linguado',
+  'ambiente',
+  'desenvolvimentismo',
+  'progresso',
+  'getulio',
+  'assoreamento',
+  'hidrodinamica',
+  'varguismo',
+  'floresta',
+  'estuario',
+  'fauna',
+  'flora',
+  'ecologia',
+  'consciente',
+  'industrializacao',
+  'vegetacao',
+  'mangue',
+  'mare',
+  'ribeirinho',
+  'aterro',
+  'pescador',
+  'sociedade',
+  'recursos',
+  'economia',
+  'oceano',
+];
+let chosenWord;
+let correctWord = '';
+let letters = [];
+let tries = 10;
+let gameOver = false;
+
+function pickRandomWord() {
+  chosenWord = words[Math.floor(Math.random() * words.length)].toUpperCase();
+  drawLines();
+  return chosenWord;
+}
+
+function checkLetter(letter) {
+  if (!gameOver) {
+    if (!letters.includes(letter)) {
+      letters.push(letter);
+      if (chosenWord.includes(letter)) {
+        for (let i = 0; i < chosenWord.length; i++) {
+          if (chosenWord[i] === letter) {
+            drawCorrectLetter(i);
+            addCorrectLetter(i);
+          }
+        }
+        checkGameOver();
+        disableKey(letter, 'correct');
+      } else {
+        tries--;
+        drawHangman();
+        checkGameOver();
+        disableKey(letter, 'incorrect');
+      }
+    }
+  }
+}
+
+function checkGameOver() {
+  if (tries == 0) {
+    // gameover
+    showEndGameText('lose', 'Fim de Jogo! Você perdeu!');
+    gameOver = true;
+  }
+  if (correctWord.length === chosenWord.length) {
+    // gamewin
+    showEndGameText('win', 'Parabéns! Você venceu!');
+    gameOver = true;
+  }
+}
+
+function addCorrectLetter(i) {
+  correctWord += chosenWord[i].toUpperCase();
+}
+
+function disableKey(key, status) {
+  //Add respective class to visually disable key
+  const keysButtons = document.querySelectorAll('.letter');
+  for (let i = 0; i < keysButtons.length; i++) {
+    if (keysButtons[i].textContent === key) {
+      keysButtons[i].classList.add(status);
+      break;
+    }
+  }
+}
+
+function addListeners() {
+  //Adds event listeners to keyboard
+  window.addEventListener('keydown', e => {
+    let letter = e.key.toUpperCase();
+    // validation if it's a letter
+    if (keys.toString().includes(letter)) {
+      checkLetter(letter);
+    }
+  });
+
+  //Adds events listeners to virtual keyboard
+  const keysButtons = document.querySelectorAll('.letter');
+  keysButtons.forEach(key =>
+    key.addEventListener('click', () => {
+      let letter = key.textContent;
+      checkLetter(letter);
+    })
+  );
+}
